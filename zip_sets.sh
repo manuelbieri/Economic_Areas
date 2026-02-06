c72155abe29d7c717e68a9ffe03dc357d6b040f3
Manuel Bieri <manuelbieri.dev@gmail.com>
2026-02-06 07:16:38 +0100

    Update content.

--- zip_sets.sh
+++ zip_sets.sh
@@ -1,6 +1,9 @@
+# Loop over all directories to create archives of sets
 for i in 'MISC' 'Missing_Links' 'UK' 'Topographie' 'Basel' 'DACH' 'Deutschland_Holland' 'Donau' 'Greater_Luxemburg' 'Katowice_Ostrava' 'Nordeuropa' 'Oberrhein' 'Overview' 'Wien_Bratislava';
 do
   zip -r ./Sets/$i.zip ./Images/$i/
   echo $i
 done
 
+# Create archive of all images
+zip -r ./Sets/images.zip ./Images/
